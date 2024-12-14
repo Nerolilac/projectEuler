1d49088652c8fe410030634115cc14c6b6340c2f
Nerolilac <pihtanaht0@gmail.com>
2024-12-14 16:18:46 +0700

    Add files via upload
    issue 1 on Project Euler .net

---
+++ issue1.nim
@@ -0,0 +1,26 @@
+import math
+
+proc mtpa3b5(n:int) :seq[int] =
+    var 
+        max = 1
+    while max != n :
+        if max mod 5 == 0 :
+            result.add(max)
+            inc max
+        elif max mod 3 == 0 :
+            result.add(max)
+            inc max
+        else :
+            inc max
+    return 
+
+let anser:seq[int] = 1000.mtpa3b5()
+
+echo  anser.sum()
+
+
+
+        
+
+
+
