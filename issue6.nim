38566acd7162bc6ae8e686c5ec271d594e203648
nerolilac <nerolilac0@outlook.com>
2024-12-18 14:38:19 +0700

    Add files via upload

---
+++ issue6.nim
@@ -0,0 +1,26 @@
+import math
+
+var 
+    a = 1
+    b:int
+    c = 2
+    d = 1
+
+while a != 101 :
+    b = b+(a^2)
+    inc a 
+
+echo "sum of squares is ", b
+
+while c != 101 :
+    d = d+c
+    inc c
+
+let 
+    e = d^2
+
+echo "square of sum is " , e 
+
+echo "diff = " , e-b
+
+
